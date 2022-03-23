a1fdc1d71bba8f77aa4ec395d583da2bd9f5777b
Nikita.Gnezdilov <Nikita.Gnezdilov@waveaccess.global>
2022-03-23 18:23:42 +0500

    +

---
+++ TestWeb/Middlewares.cs
@@ -0,0 +1,24 @@
+﻿namespace TestWeb
+{
+    public static class Middlewares
+    {
+
+        public class CustomMW
+        {
+            RequestDelegate next;
+            string kek;
+
+            public CustomMW(RequestDelegate next, string kek)
+            {
+                this.next = next;
+                this.kek = kek;
+            }
+
+            public async Task InvokeAsync(HttpContext context)
+            {
+                await context.Response.WriteAsync(kek);
+            }
+        }
+
+    }
+}
--- TestWeb/Program.cs
+++ TestWeb/Program.cs
@@ -1,22 +1,12 @@
-using Microsoft.AspNetCore.Http;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-app.Run(MW);
-
-async Task MW(HttpContext context)
-{
-    await context.Response.WriteAsync("kek");
-}
 
 app.UseHttpsRedirection();
 
